88dffe7897cfe0632be0047c0b9849618511c906
gachiboss <60434851+gachiboss@users.noreply.github.com>
2020-02-12 13:01:08 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = 'Njc2ODcxMDcwMjM3NzIwNjE3.XkMFSw.rEN4W6iwlF-8UL-ONI4tqZkXzfU'
+
+POST_ID = 676886995196313605 #POST ID TO READ REACTIONS FROM
+
+#roles list according to emotes
+ROLES = {'😀' : 676884510184439839,
+'😇' : 676884563284328453, '😈' : 676884602760986647, '😎' : 676884650945413176}
+
+#exclude this roles from counting
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 10
